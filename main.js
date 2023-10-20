128c4e74686bce73ca5a2eaa38a4a80197f2ddb7
ShauryaUdharaju <118858893+ShauryaUdharaju@users.noreply.github.com>
2023-10-19 21:04:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var first_name="Shaurya";
+var last_name="Udharaju";
+var full_name=first_name.concat(last_name);
+console.log(full_name)
+
+var a=10;
+var b=5;
+
+var add=a+b;
+console.log(add);
+
+var minus=a-b;
+console.log(minus);
+
+var multiple=a*b;
+console.log(multiple);
+
+var divide=a/b;
+console.log(divide);
